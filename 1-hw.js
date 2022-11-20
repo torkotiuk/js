4c0000575feed004a12ec2ea54f336d7d71f7e4f
TAL <torkotiuk@gmail.com>
2022-11-20 11:14:08 +0100

    finish task 6 modul 1

--- 1-hw.js
+++ 1-hw.js
@@ -47,41 +47,68 @@
 //   }
 // }
 
-const someCountry = prompt('Enter your country.');
-if (someCountry === null) {
-  console.log('Cancelled by the user');
-} else if (someCountry[0] === undefined) {
-  console.log('--- empty enter ---');
-  console.log('We do not delivery to your country.');
-} else {
-  const firstLetter = someCountry[0].toUpperCase();
-  const fromSecondLetter = someCountry
-    .slice(1, someCountry.length)
-    .toLowerCase();
-  const country = firstLetter + fromSecondLetter;
-  let price = 0;
-  switch (country) {
-    case 'China':
-      price = 100;
-      console.log(`Delivery to ${country} will be cost ${price} credits`);
-      break;
-    case 'Chile':
-      price = 250;
-      console.log(`Delivery to ${country} will be cost ${price} credits`);
-      break;
-    case 'Australia':
-      price = 170;
-      console.log(`Delivery to ${country} will be cost ${price} credits`);
-      break;
-    case 'India':
-      price = 80;
-      console.log(`Delivery to ${country} will be cost ${price} credits`);
-      break;
-    case 'Jamaica':
-      price = 120;
-      console.log(`Delivery to ${country} will be cost ${price} credits`);
-      break;
-    default:
-      console.log('We do not delivery to your country.');
+// task 5
+// const someCountry = prompt('Enter your country.');
+// let message;
+// if (someCountry === null) {
+//   message = 'Cancelled by the user';
+// } else if (someCountry[0] === undefined) {
+//   console.log('--- empty enter ---');
+//   message = 'We do not delivery to your country.';
+// } else {
+//   const firstLetter = someCountry[0].toUpperCase();
+//   const fromSecondLetter = someCountry
+//     .slice(1, someCountry.length)
+//     .toLowerCase();
+//   const country = firstLetter + fromSecondLetter;
+//   let price = 0;
+//   switch (country) {
+//     case 'China':
+//       price = 100;
+//       message = `Delivery to ${country} will be cost ${price} credits`;
+//       break;
+//     case 'Chile':
+//       price = 250;
+//       message = `Delivery to ${country} will be cost ${price} credits`;
+//       break;
+//     case 'Australia':
+//       price = 170;
+//       message = `Delivery to ${country} will be cost ${price} credits`;
+//       break;
+//     case 'India':
+//       price = 80;
+//       message = `Delivery to ${country} will be cost ${price} credits`;
+//       break;
+//     case 'Jamaica':
+//       price = 120;
+//       message = `Delivery to ${country} will be cost ${price} credits`;
+//       break;
+//     default:
+//       message = 'We do not delivery to your country.';
+//   }
+// }
+// alert(message);
+
+// task 6
+let total = 0;
+let input;
+let isEmptyString;
+
+console.log('Before cycle');
+for (let i = 1; i < Infinity; i += 1) {
+  console.log(`---- Step ${i} -----`);
+  input = prompt('Please enter the number to add or "Cancel/Escape" to sum');
+  isEmptyString = input;
+  input = Number(input);
+
+  if (input) {
+    total += input;
+    console.log('Adding: ', input);
+  } else if (isNaN(input) || isEmptyString === '') {
+    alert('You entered not a number, please enter a number.');
+  } else {
+    console.log('Break');
+    alert(`Total: ${total}`);
+    break;
   }
 }
