4517763e62b3d1dbda81d4b70b7b24c4cb62bb3f
TAL <torkotiuk@gmail.com>
2022-11-21 10:12:25 +0100

    add burger menu

---
+++ js/burger-menu.js
@@ -0,0 +1,9 @@
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', () => {
+    menuBtnRef.classList.toggle('is-open');
+    mobileMenuRef.classList.toggle('is-open');
+  });
+})();
